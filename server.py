5d210a814436d174935b601a2285a040e2a83fbb
Julien Stilmant <stilmantjulien@hotmail.com>
2016-02-17 18:59:17 +0100

    Add hello world bottle-based web server

--- server.py
+++ server.py
@@ -8,6 +8,51 @@ from bottle import route, run
 
 @route('/')
 def home():
-    return 'Hello World!'
+    c='''
+        <!DOCTYPE html>
+        <html>
+        <head lang="en">
+            <meta charset="UTF-8">
+            <title> Le salon de la bière </title>
+            <style>
+                body{
+                    background-image:url("http://photos.up-wallpaper.com/images248/euj0c1zssmt.jpg");
+                    background-repeat: no repeat;
+                    background-size:cover;
+                }
+                #indentificateur{
+                    color:white;
+                    text-align:center;
+                }
+                h1{
+                    color:white;
+                    font-size:50px;
+                    text-align:center;
+                }
+                a{
+                    color:white;
+                }
+                a:hover{
+                    color:rgb(240,245,0);
+                }
+
+
+            </style>
+        </head>
+        <body>
+            <h1>Bienvenue au salon de bière</h1>
+            <form method="post" action="/login" id="indentificateur">
+                <p>
+                <label for="Identifiant">Votre pseudo</label> : <input type="text" name="pseudo" />
+                <br/>
+                <label for="pass">Mot de passe</label> : <input type="password" name="pass" />
+            </p>
+            <p><a href="http://localhost:8088/sinscrire"> Pas encore de compte ? </p>
+            <input type="submit" value = 'se connecter'/>
+            </form>
+
+        </body>
+        </html>'''
+    return c
 
 run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
